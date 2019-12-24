d6664f98ffde789c84ae0b983f80be37fa1fea3d
Nomade <nomade040@gmail.com>
2019-12-23 22:42:30 -0300

    Fixed opcodes A0H-A3H not being parsed correctly in the presence of a operand-size override prefix

--- ldisasm.h
+++ ldisasm.h
@@ -13,33 +13,34 @@ static constexpr uint8_t op2modrm[] = { 0x0D, 0xA3, 0xA4, 0xA5, 0xAB, 0xAC, 0xAD
 
 size_t ldisasm(const void* const address)
 {
-	size_t offset = 0;
+	size_t offset = 0; 
 	bool operandPrefix = false, addressPrefix = false, rexW = false;
 	const uint8_t* b = reinterpret_cast<const uint8_t*>(address);
 
 	auto parseModRM = [&]() {
 		uint8_t modrm = *++b;
 
-		if (!addressPrefix || (addressPrefix && *b >= 0x40))
-		{
+		if (!addressPrefix || (addressPrefix && *b >= 0x40)) {
 			bool hasSIB = false; //Check for SIB byte
 			if (*b < 0xC0 && (*b & 0b111) == 0b100 && !addressPrefix)
 				hasSIB = true, b++;
 
 			if (modrm >= 0x40 && modrm <= 0x7F) // disp8 (ModR/M)
 				b++;
-			else if ((modrm <= 0x3F && (modrm & 0b111) == 0b101) || (modrm >= 0x80 && modrm <= 0xBF)) //disp32 (ModR/M)
+			else if ((modrm <= 0x3F && (modrm & 0b111) == 0b101) || (modrm >= 0x80 && modrm <= 0xBF)) //disp16,32 (ModR/M)
 				b += (addressPrefix) ? 2 : 4;
 			else if (hasSIB && (*b & 0b111) == 0b101) //disp8,32 (SIB)
 				b += (modrm & 0b01000000) ? 1 : 4;
 		}
+		else if (addressPrefix && modrm == 0x26)
+			b += 2;
 	};
 
 	//Parse legacy prefixes & REX prefixes
 #ifdef _WIN64
-		for (int i = 0; i < 15 && (find(prefixes, *b) || LDISM_R == 4); i++, b++)
+	for (int i = 0; i < 15 && (find(prefixes, *b) || LDISM_R == 4); i++, b++)
 #else
-		for (int i = 0; i < 15 && find(prefixes, *b); i++, b++)
+	for (int i = 0; i < 15 && find(prefixes, *b); i++, b++)
 #endif
 	{
 		if (*b == 0x66)
@@ -66,7 +67,7 @@ size_t ldisasm(const void* const address)
 		{
 			if (LDISM_R == 8) //disp32
 				offset += 4;
-			else if ((LDISM_R == 7 && LDISM_C < 4) || *b == 0xA4 || *b == 0xC2 || (*b > 0xC3 && *b <= 0xC6) || *b == 0xBA) //imm8
+			else if ((LDISM_R == 7 && LDISM_C < 4) || *b == 0xA4 || *b == 0xC2 || (*b > 0xC3 && *b <= 0xC6) || *b == 0xBA || *b == 0xAC) //imm8
 				offset++;
 
 			//Check for ModR/M, SIB and displacement
@@ -83,8 +84,10 @@ size_t ldisasm(const void* const address)
 			offset += 2;
 		else if (*b == 0xC8) //imm16 + imm8
 			offset += 3;
-		else if ((LDISM_R < 4 && (LDISM_C == 5 || LDISM_C == 0xD)) || (LDISM_R == 0xA && LDISM_C < 4) || (LDISM_R == 0xB && LDISM_C >= 8) || (*b == 0xF7 && !(*(b + 1) & 56)) || find(op1imm32, *b)) //imm32,16
-			offset += (rexW) ? 8 : ((operandPrefix || addressPrefix) ? 2 : 4);
+		else if ((LDISM_R < 4 && (LDISM_C == 5 || LDISM_C == 0xD)) || (LDISM_R == 0xB && LDISM_C >= 8) || (*b == 0xF7 && !(*(b + 1) & 56)) || find(op1imm32, *b)) //imm32,16
+			offset += (rexW) ? 8 : (operandPrefix ? 2 : 4);
+		else if (LDISM_R == 0xA && LDISM_C < 4)
+			offset += (rexW) ? 8 : (addressPrefix ? 2 : 4);
 		else if (*b == 0xEA || *b == 0x9A) //imm32,48
 			offset += operandPrefix ? 4 : 6;
 
